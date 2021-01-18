a3de83a921ca9b65f6fcd4f64db4a7720b76c44e
BipinKalra <bipinkalra@gmail.com>
2021-01-18 16:43:55 +0530

    keypress event

--- script.js
+++ script.js
@@ -22,11 +22,21 @@ for (let i = 0; i < buttonsOpenModal.length; i++) {
 //   overlay.classList.add("hidden");
 // });
 
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 // A way to add the same event listener to two elements simultaneously where elements don't share the same class
 // This can also be done by making a function closeModal and then passing it to the two event listeners
 [buttonCloseModal, overlay].forEach(function (item) {
-  item.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
+  item.addEventListener("click", closeModal);
+});
+
+document.addEventListener("keydown", function (event) {
+  // console.log(event);
+
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) closeModal();
+  }
 });
