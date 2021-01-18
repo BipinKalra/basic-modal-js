acb0aae2c81ade303f54354451904b285c41eec4
BipinKalra <bipinkalra@gmail.com>
2021-01-18 16:17:24 +0530

    add: functionality

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const buttonCloseModal = document.querySelector(".close-modal");
+const buttonsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  buttonsOpenModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+buttonCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+if (!overlay.classList.contains("hidden")) {
+  overlay.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+}
