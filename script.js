207632c9da1d441b06ce02c976d0019895b8bd8c
BipinKalra <bipinkalra@gmail.com>
2021-01-18 16:30:08 +0530

    change: restructuring

--- script.js
+++ script.js
@@ -12,14 +12,21 @@ for (let i = 0; i < buttonsOpenModal.length; i++) {
   });
 }
 
-buttonCloseModal.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+// buttonCloseModal.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
 
-if (!overlay.classList.contains("hidden")) {
-  overlay.addEventListener("click", function () {
+// overlay.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// A way to add the same event listener to two elements simultaneously where elements don't share the same class
+// This can also be done by making a function closeModal and then passing it to the two event listeners
+[buttonCloseModal, overlay].forEach(function (item) {
+  item.addEventListener("click", function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
   });
-}
+});
